251f3e675608f944fac1604b295e7cbfaab171d1
Elena Piaggio <elenapiaggio@gmail.com>
2020-07-09 10:32:46 +0200

    UDPATE Employee class

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -2,25 +2,20 @@ package domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.util.Date;
+
 @Data
+@Entity
+@Table(name="employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String surname;
     private String job;
-    private static int nextuniqueID = 0;
-    private int ID;
-
-    public Employee(){
-        nextuniqueID++;
-        ID = nextuniqueID;
-    }
-
-    public Employee(String name, String surname, String job){
-        this.name = name;
-        this.surname = surname;
-        this.job = job;
-        nextuniqueID++;
-        ID = nextuniqueID;
-    }
-
+    @Column(name="create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
 }
