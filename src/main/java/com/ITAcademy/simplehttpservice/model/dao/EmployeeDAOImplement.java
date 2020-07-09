7956d16a51ae23e0093222388a6990c1c11a672f
Elena Piaggio <elenapiaggio@gmail.com>
2020-07-09 19:41:29 +0200

    UPDATE persistence

--- src/main/java/com/ITAcademy/simplehttpservice/model/dao/EmployeeDAOImplement.java
+++ src/main/java/com/ITAcademy/simplehttpservice/model/dao/EmployeeDAOImplement.java
@@ -1,14 +1,22 @@
 package com.ITAcademy.simplehttpservice.model.dao;
 
 import com.ITAcademy.simplehttpservice.model.entity.Employee;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Component
+@Repository
 public class EmployeeDAOImplement implements IEmployeeDAO{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional(readOnly = true)
     @Override
     public List<Employee> findAll() {
-        return null;
+        return em.createQuery("from Employee").getResultList();
     }
 }
